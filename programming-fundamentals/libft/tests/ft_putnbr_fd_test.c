602c74790f528fd5f2f4d187fc12b8d4f17ab31d
Abderahmane BEHAR RAHALA <abehar-r@c2r10s1.marseille.42.school>
2025-11-26 22:06:26 +0100

    42

--- programming-fundamentals/libft/tests/ft_putnbr_fd_test.c
+++ programming-fundamentals/libft/tests/ft_putnbr_fd_test.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putnbr_fd_test.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abehar-r <abderahmane.beharrahala@learner  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 18:19:14 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/26 21:15:56 by abehar-r         ###   ########.fr       */
+/*   Created: 2025/11/26 22:01:11 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/26 22:04:23 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "../include/libft.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+void	ft_putnbr_fd_test(void)
+{
+	ft_putstr_fd("\n - ft_putnbr(): ", 1);
+	int	nbr;
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putchar_fd_test(void);
-
-void	ft_putstr_fd(char *str, int fd);
-void	ft_putstr_fd_test(void);
-
-void	ft_putnbr_fd(int nbr, int fd);
-void	ft_putnbr_fd_test(void);
-
-#endif
+	nbr = 42;
+	ft_putnbr_fd(nbr, 1);
+}
