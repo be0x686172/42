8bfe53fb8ad1bd769404b6702d05f3307c9fa145
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-28 13:47:30 +0100

    42

--- programming-fundamentals/libft/tests/ft_strlen_test.c
+++ programming-fundamentals/libft/tests/ft_strlen_test.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen_test.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abehar-r <abderahmane.beharrahala@learner  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 18:19:14 by abehar-r          #+#    #+#             */
-/*   Updated: 2025/11/26 21:15:56 by abehar-r         ###   ########.fr       */
+/*   Created: 2025/11/28 13:42:25 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/28 13:46:55 by abehar-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_TESTER_H
-# define LIBFT_TESTER_H
+#include "../include/libft_tester.h"
 
-# include "../libft/include/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+void	ft_strlen_test(void)
+{
+	ft_putstr_fd("\n - ft_strlen(): ", 1);
+	char	*str;
+	size_t	len;
 
-void	ft_putchar_fd_test(void);
-void	ft_putstr_fd_test(void);
-void	ft_putnbr_fd_test(void);
-void	ft_strlen_test(void);
-
-#endif
+	str = "Jean Perrin High School";
+	len = ft_strlen(str);
+	ft_putnbr_fd((int)len, 1);
+}
