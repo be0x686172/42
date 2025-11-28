4889513576b7dd76a53a4a68efee1b3243f5f7f4
Abderahmane BEHAR RAHALA <abehar-r@c1r17s3.marseille.42.school>
2025-11-28 19:16:25 +0100

    42

---
+++ programming-fundamentals/printf/include/ft_printf_tester.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_tester.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abehar-r <abderahmane.beharrahala@learner  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 17:31:27 by abehar-r          #+#    #+#             */
+/*   Updated: 2025/11/28 17:36:04 by abehar-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_TESTER_H
+# define FT_PRINTF_TESTER_H
+
+# include "../printf/include/ft_printf.h"
+
+#endif
